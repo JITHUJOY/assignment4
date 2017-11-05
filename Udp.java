d18c47381dc1109f808302291632889f80812574
JITHUJOY <33262377+JITHUJOY@users.noreply.github.com>
2017-11-06 01:16:57 +0530

    Add files via upload

---
+++ Udp.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udp;
+
+/**
+ *
+ * @author Nuclieus
+ */
+public class Udp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     chatserver ct=new chatserver();
+     chatclient cc=new chatclient();
+   
+        // TODO code application logic here
+    }
+    
+}
